11af7ef4a04e001dc97c539f0d08749b79dbe7d3
codlearete <etkatharpi01@gmail.com>
2023-04-06 18:37:46 +0530

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+function Todo(){
+    return(
+        <div>
+            <h1>My Todo</h1>
+            <div className="card">
+                <h2 >TITLE</h2>
+                <div className="actions">
+                    <button className="btn">Delete</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Todo
